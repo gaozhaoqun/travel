ab0e8e434aad08b2f4bab16cb78548d29daec153
gaozhaoqun <2855521@qq.com>
2018-08-07 23:40:22 +0800

    router test

---
+++ my-vue-travel/src/components/list.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        {{ msg }}
+        <br>
+        <router-link to="/">go home</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            msg: "list页面"
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
