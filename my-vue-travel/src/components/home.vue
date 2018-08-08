7faa8b031029e4796d8bb5e0b12a57e233ff1165
gaozhaoqun <2855521@qq.com>
2018-08-08 13:04:39 +0800

    拉完代码 本地多了这些

---
+++ my-vue-travel/src/components/home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div> 
+    {{ msg }} 
+    <br>
+    <router-link to="/List">go list</router-link>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+<style scoped></style>
---
+++ my-vue-travel/src/router/index.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/components/home'
+import List from '@/components/list'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '/List',
+      name: 'List',
+      component: List
+    }
+  ]
+})
