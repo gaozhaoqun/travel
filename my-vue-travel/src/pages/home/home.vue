3c0da02b06d3d2dc0d7e72ed8c1f364fb5bb6fd0
gaozhaoqun <2855521@qq.com>
2018-08-13 23:18:53 +0800

    8月13日提交: header组件的开发(reset.css未补全)

---
+++ my-vue-travel/src/pages/home/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div> 
+    <home-header></home-header>
+  </div>
+</template>
+<script>
+import HomeHeader from './components/Header'  // 引入子组件 ,然后记得注册子组件
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader  // ES6写法, 键和值一样的话 , 可以这样简写
+  }
+}
+</script>
+<style scoped></style>
