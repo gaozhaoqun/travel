08492999b091c5ff1bd53e7febbdf0c2f5b3d121
gaozhaoqun <2855521@qq.com>
2018-08-17 10:28:33 +0800

    2018-08-17 10:30 : 提交引入图片定义为数据 require(url),

--- my-vue-travel/src/pages/home/components/Header.vue
+++ my-vue-travel/src/pages/home/components/Header.vue
@@ -15,38 +15,36 @@ export default {
 <style scoped lang="stylus">
   /* 在样式中 用@符引, 前面一定要加 ~  */
   /*  @import '~@/assets/styles/varibles.styl'  */
-  @import '~styles/varibles.styl'
+  @import '~styles/varibles.styl'   // 引用styles目录下的 varibles.styl
   .header 
-    display: flex;
-    line-height: 0.86rem;
-    background: $bgColor
-    color: #fff;
+    display: flex
+    line-height: 0.86rem
+    background: $bgColor    // 使用stylus变量
+    color: #fff
 
     .header-left 
-      width: 0.84rem;
-      float: left;
-      text-align center;
+      width: 0.84rem
+      float: left
+      text-align center
       .back-icon
         text-align center
         font-size .4rem
-    
 
     .header-input 
-      flex: 1;
-      background: #fff;
-      color: #333;
+      flex: 1
+      background: #fff
+      color: #333
       margin-top .12rem
       height .64rem
       color #ccc
       line-height .64rem
       border-radius .1rem
       text-indent .2rem
-    
 
     .header-right 
-      width: 1.24rem;
-      float: right;
-      text-align: center;
+      width: 1.24rem
+      float: right
+      text-align: center
       .arrow-icon
         font-size .2rem
 </style>
--- my-vue-travel/src/pages/home/components/swiper.vue
+++ my-vue-travel/src/pages/home/components/swiper.vue
@@ -3,7 +3,7 @@
         <swiper :options="swiperOption">
             <!-- slides -->
             <swiper-slide v-for="item of swiperList" :key="item.id">
-                <img class="swiper-img" :src="item.imgUrl" alt="">
+                <img class="swiper-img" :src="item.imgUrl">
             </swiper-slide>
             <!-- Optional controls -->
             <div class="swiper-pagination"  slot="pagination"></div>
@@ -20,12 +20,15 @@ export default {
     data () {
         return {
            swiperOption: {
-               pagination: '.swiper-pagination',
+               pagination: '.swiper-pagination',  // 白色点点分页符
                loop: true  // 循环轮播
            },
            swiperList: [
-               {id: '001',imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/20180716_wap_11043.jpg'},
-               {id: '002',imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/dujia_shuqi_banner_20180710.jpg'}
+               // {id: '001',imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/20180716_wap_11043.jpg'},
+               // {id: '002',imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/dujia_shuqi_banner_20180710.jpg'}
+               {id: '001',imgUrl: require('@/assets/img/rottweiler1.jpg')},  // 引用本地图片要用 require('图片路径')
+               {id: '002',imgUrl: require('@/assets/img/rottweiler2.jpg')}
+
            ]
         }
     }
@@ -38,19 +41,19 @@ export default {
         background: #fff!important
     /*
      template模板里面,最外层加div class=wrapper 是为了图片未显示的时候  撑高的
-     下面的 .wrapper 里的padding-bottom[假设图片宽640, 高度是200, 用高除以宽度的结果].)
+     下面的 .wrapper 里的padding-bottom[假设图片宽750, 高度是184, 用高除以宽度的结果].)
         height 0
         overflow hidden
-        padding-bottom 31.25%
+        padding-bottom: 24.53%;
     */
     .wrapper
-        width 7.5rem
+        width 100%
         height 0
         // overflow hidden
-        padding-bottom 31.25%
+        padding-bottom: 24.53%
         background #eee
         .swiper-img
-            width 7.5rem
-            height 2.3rem
+            width 100%
+            height 4.3rem
         
 </style>
