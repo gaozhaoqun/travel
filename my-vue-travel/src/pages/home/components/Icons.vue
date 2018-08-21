5124e0a798315dd3512d4ca44d485e379724ffaf
gaozhaoqun <2855521@qq.com>
2018-08-21 22:53:36 +0800

    2018-08-21 22:00提交 : icon区的轮播搞定, 计算属性computed仔细看,

--- my-vue-travel/src/pages/home/components/Icons.vue
+++ my-vue-travel/src/pages/home/components/Icons.vue
@@ -1,17 +1,15 @@
 <template>
     <div class='icons'>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="@/assets/img/icon1.jpg" />
-                <p class="icon-desc">热门景点</p>
-            </div>
-        </div>
-        <div class="icon">
-            <div class="icon-img">
-                <img class="icon-img-content" src="@/assets/img/icon1.jpg" />
-                <p class="icon-desc">热门景点</p>
-            </div>
-        </div>
+        <swiper :options="swiperOption">
+            <swiper-slide  v-for="(page, index) of pages" :key="index">
+                <div class="icon" v-for="item of page" :key="item.id">
+                    <div class="icon-img">
+                        <img class="icon-img-content" :src="item.imgUrl" />
+                        <p class="icon-desc"> {{ item.desc }} </p>
+                    </div>
+                </div>
+            </swiper-slide>
+        </swiper>
     </div>
 </template>
 
@@ -20,7 +18,37 @@ export default {
     name: 'HomeIcons',
     data () {
         return {
-            
+            swiperOption: {
+               loop: false  // 循环轮播
+           },
+           iconList: [
+               {id: '001', imgUrl: require('@/assets/img/icon2.jpg'), desc: '我是糯米'},
+               {id: '002', imgUrl: require('@/assets/img/icon1.jpg'), desc: '我是途牛'},
+               {id: '003', imgUrl: require('@/assets/img/icon1.jpg'), desc: '我是途牛'},
+               {id: '004', imgUrl: require('@/assets/img/icon1.jpg'), desc: '我是途牛'},
+               {id: '005', imgUrl: require('@/assets/img/icon2.jpg'), desc: '我是糯米'},
+               {id: '006', imgUrl: require('@/assets/img/icon2.jpg'), desc: '我是糯米'},
+               {id: '007', imgUrl: require('@/assets/img/icon1.jpg'), desc: '我是途牛'},
+               {id: '008', imgUrl: require('@/assets/img/icon2.jpg'), desc: '我是糯米'},
+               {id: '009', imgUrl: require('@/assets/img/icon1.jpg'), desc: '我是途牛'},
+               {id: '010', imgUrl: require('@/assets/img/icon2.jpg'), desc: '我是糯米'},
+               {id: '011', imgUrl: require('@/assets/img/icon1.jpg'), desc: '我是途牛'},
+               {id: '012', imgUrl: require('@/assets/img/icon2.jpg'), desc: '我是糯米'},
+               {id: '012', imgUrl: require('@/assets/img/icon2.jpg'), desc: '我是糯米'}
+           ]
+        }
+    },
+    computed: {
+        pages () {
+            const pages = []
+            this.iconList.forEach((item,index) => {
+                const page = Math.floor(index / 8)
+                if (!pages[page]) {
+                    pages[page] = []
+                }
+                pages[page].push(item)
+            })
+            return pages
         }
     }
 }
@@ -28,13 +56,16 @@ export default {
 
 <style lang="stylus" scoped>
     @import '~styles/varibles.styl'
+    .icons >>> .swiper-container{
+        padding-bottom 25%
+    }
     .icons {
         height: 0
         overflow: hidden
         padding-bottom: 50%
         .icon {
             position relative
-            float: left
+            float left
             width: 25%
             height 0
             padding-bottom: 25%
@@ -62,6 +93,7 @@ export default {
                 text-align center
                 color $c333
                 width 100%
+                ellipsis()
             }
         }
     }
