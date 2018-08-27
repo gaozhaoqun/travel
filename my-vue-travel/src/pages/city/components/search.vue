7a9f92c74e108cd8dbf9828d4fc69ec93bd324f8
gaozhaoqun <2855521@qq.com>
2018-08-27 23:21:30 +0800

    搜索框开发完了, 开始看8 - 3

---
+++ my-vue-travel/src/pages/city/components/search.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="search">
+        <input class="searchInput" type="text" placeholder="輸入城市名或拼音" />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl';
+    .search
+        height: .72rem
+        background: $bgColor
+        padding: .1rem
+        .searchInput
+            box-sizing border-box
+            color: #666
+            height .72rem
+            line-height .72rem
+            width 100%
+            border-radius .06rem
+            padding: .1rem
+</style>
