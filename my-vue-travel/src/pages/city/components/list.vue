d82d8765602743a04c877c789018977617a4d361
gaozhaoqun <2855521@qq.com>
2018-08-28 19:55:46 +0800

    8-28 20:00提交 => 城市选择列表页面, .list postion: absolute t1.78rem r0 l0 b0 overflow: hidden,是为了让首页宽高整屏

---
+++ my-vue-travel/src/pages/city/components/list.vue
@@ -0,0 +1,138 @@
+<template>
+    <div>
+        <div class="list">
+            <div class="area">
+                <div class="title border-topbottom">当前城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper">
+                        <div class="button">深圳</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">深圳</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">深圳</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">深圳</div>
+                    </div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">热门城市</div>
+                <div class="button-list">
+                    <div class="button-wrapper">
+                        <div class="button">深圳</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">深圳</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">深圳</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">深圳</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">深圳</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">深圳</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">深圳</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">深圳</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">深圳</div>
+                    </div>
+                    <div class="button-wrapper">
+                        <div class="button">深圳</div>
+                    </div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">A</div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">B</div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                </div>
+            </div>
+            <div class="area">
+                <div class="title border-topbottom">C</div>
+                <div class="item-list">
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                    <div class="item border-bottom">阿拉善</div>
+                </div>
+            </div>
+        </div>
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'CityList'
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl';
+    .border-topbottom:before,.border-topbottom:after,.border-bottom:before
+        border-color: #ccc
+    .list
+        position absolute
+        top 1.78rem
+        right 0
+        bottom 0
+        left 0
+        overflow hidden
+        .title 
+            line-height: .44rem
+            font-size: .26rem
+            background-color: #eee
+            padding-left: .2rem
+            color: #666
+        .button-list
+            padding: .1rem .6rem .1rem .1rem
+            overflow hidden
+            .button-wrapper
+                float: left
+                width: 33.33%
+                .button
+                    text-align center
+                    padding: .1rem 0
+                    margin .1rem
+                    border: .02rem solid #ccc
+                    border-radius: .06rem
+        .item-list
+            .item
+                line-height: .76rem
+                padding-left: .2rem
+</style>
