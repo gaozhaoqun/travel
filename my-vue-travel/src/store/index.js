77e169d4df3b79b86c4b6675f634f2cae625b614
gaozhaoqun <2855521@qq.com>
2018-09-04 13:42:34 +0800

    Vuex使用步驟

--- my-vue-travel/src/store/index.js
+++ my-vue-travel/src/store/index.js
@@ -18,3 +18,35 @@ export default new Vuex.Store({
         }
     }
 })
+
+/*
+    1. 安装
+    npm i vuex --save 
+     2. src目录下新建文件夹 store / index.js
+     3. 编辑 index.js
+        imporm Vue from 'vue'
+        import Vuex from 'vuex'
+        Vue.use(Vuex)
+        export default new Vuex.Store({
+            state: {city: '深圳'}
+        })
+        mutations: {
+            changeCity (state, city) {  // 参数是监听别的组件 传入进来的city
+                state.city = city  // 把从别的组件接收来的数据, 赋值给state.city
+            }
+        }
+    4. 在组件里使用
+        在标签栏定义个方法 handleCityClick(item.name)
+        methods: {
+            handleCityClick (city) {
+                this.$store.dispatch('changeCity, city)
+            }
+        }
+     5. Vue的页面跳转
+    methods: {
+            handleCityClick (city) {
+                this.$store.dispatch('changeCity', city) // 把选中的值, 传给state
+                this.$router.push('/')  // 跳转到首页
+            }
+        }
+*/ 
